de68993603a953c22b0c42a25d7321aeac0f1613
DisRachik <radchenkoig@gmail.com>
2023-12-27 18:04:26 +0200

    initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log('first');
-const foo = 'next';
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i += 1) {
+      for (let j = 0; j < length - i - 1; j += 1) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([54, -3, 13, -5]);
+sorter.sort();
+console.log(sorter.collection);
