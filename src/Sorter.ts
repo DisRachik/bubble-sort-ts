787bd58986baedd3668394a0953e558d53afcfbe
DisRachik <radchenkoig@gmail.com>
2024-02-13 16:09:20 +0200

    refactoring: use abstract class for different databases

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,19 +1,15 @@
-interface Sortable {
-  length: number;
-  compere(leftInd: number, rightInd: number): boolean;
-  change(leftInd: number, rightInd: number): void;
-}
-
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract compere(leftInd: number, rightInd: number): boolean;
+  abstract change(leftInd: number, rightInd: number): void;
+  abstract length: number;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i += 1) {
       for (let j = 0; j < length - i - 1; j += 1) {
-        if (this.collection.compere(j, j + 1)) {
-          this.collection.change(j, j + 1);
+        if (this.compere(j, j + 1)) {
+          this.change(j, j + 1);
         }
       }
     }
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { LinkedList } from './LinkedList';
+
 const numbersCollection = new NumbersCollection([54, -3, 13, -5]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('iHor');
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -17,6 +16,6 @@ linkedList.add(55);
 linkedList.add(73);
 linkedList.add(8);
 linkedList.add(-4);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+
+linkedList.sort();
 linkedList.print();
